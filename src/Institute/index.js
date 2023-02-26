306f752dbebb8156682aa03dc42ff94d5b6a48e6
bootsrap <adhiltajudeen@gmail.com>
2023-02-26 12:53:43 +0530

    clear project

--- src/Institute/index.js
+++
@@ -1,4 +0,0 @@
-export * from './Api'
-export * from './Containers'
-export * from './hooks'
-export * from './Component'
--- src/index.js
+++ src/index.js
@@ -2,18 +2,11 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import { QueryClient, QueryClientProvider } from "react-query";
 import "bootstrap/dist/css/bootstrap.min.css";
-import Institute from "./Institute/Institute";
+
 const queryClient = new QueryClient();
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <QueryClientProvider client={queryClient}>
-    <Institute />
+    <CrudOp />
   </QueryClientProvider>
 );
-// import CourseApi from "./Institute/Containers/CourseApi";
-// import CourseApi from './Institute/Containers/CourseApi'
-// import CustomApp from './CustomApp';
-// import App from './App';
-// <App />
-// <CustomApp/>
-// <NewApp/>
